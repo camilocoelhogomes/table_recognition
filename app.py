0d55220ccca4b68ad66ae4d96a5e22e9523108de
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-06-19 07:46:31 -0300

    connect to aws bucket

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hello World")
+import boto3
+s3 = boto3.resource('s3')
+reader = boto3.client('rekognition')
+
+
+def list_files():
+    files = []
+    bucket = s3.Bucket('test-bucket-camilo')
+    for file in bucket.objects.all():
+        files.append(file.key)
+    return files
+
+
+def detect_text(images):
+    response = reader.detect_text(
+        Image={'S3Object': images[0]})
+    print(response)
+    return response
+
+
+images = list_files()
+images
