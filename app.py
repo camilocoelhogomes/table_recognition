a00ef238fac4a387f0f095bbc93e2dbd0c07de77
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-06-21 09:15:23 -0300

    remove the textract routines from app

--- app.py
+++ app.py
@@ -1,99 +1,9 @@
 from unicodedata import name
-from flask import Flask, request, send_file
+from flask import Flask, Response, request, send_file
 from flask_cors import CORS, cross_origin
 import base64
-import boto3
 import os
-
-
-def get_rows_columns_map(table_result, blocks_map):
-    rows = {}
-    for relationship in table_result['Relationships']:
-        if relationship['Type'] == 'CHILD':
-            for child_id in relationship['Ids']:
-                cell = blocks_map[child_id]
-                if cell['BlockType'] == 'CELL':
-                    row_index = cell['RowIndex']
-                    col_index = cell['ColumnIndex']
-                    if row_index not in rows:
-                        # create new row
-                        rows[row_index] = {}
-
-                    # get the text value
-                    rows[row_index][col_index] = get_text(cell, blocks_map)
-    return rows
-
-
-def get_text(result, blocks_map):
-    text = ''
-    if 'Relationships' in result:
-        for relationship in result['Relationships']:
-            if relationship['Type'] == 'CHILD':
-                for child_id in relationship['Ids']:
-                    word = blocks_map[child_id]
-                    if word['BlockType'] == 'WORD':
-                        text += word['Text'] + ' '
-                    if word['BlockType'] == 'SELECTION_ELEMENT':
-                        if word['SelectionStatus'] == 'SELECTED':
-                            text += 'X '
-    return text
-
-
-def get_table_csv_results(file_name):
-    client = boto3.client('textract')
-
-    response = client.analyze_document(
-        Document={'Bytes': file_name}, FeatureTypes=['TABLES'])
-
-    # Get the text blocks
-    blocks = response['Blocks']
-
-    blocks_map = {}
-    table_blocks = []
-    for block in blocks:
-        blocks_map[block['Id']] = block
-        if block['BlockType'] == "TABLE":
-            table_blocks.append(block)
-
-    if len(table_blocks) <= 0:
-        return "<b> NO Table FOUND </b>"
-
-    csv = ''
-    for index, table in enumerate(table_blocks):
-        csv += generate_table_csv(table, blocks_map, index + 1)
-        csv += '\n\n'
-
-    return csv
-
-
-def generate_table_csv(table_result, blocks_map, table_index):
-    rows = get_rows_columns_map(table_result, blocks_map)
-
-    table_id = 'Table_' + str(table_index)
-
-    # get cells.
-    csv = 'Table: {0}\n\n'.format(table_id)
-
-    for row_index, cols in rows.items():
-
-        for col_index, text in cols.items():
-            csv += '{}'.format(text) + ";"
-        csv += '\n'
-
-    csv += '\n\n\n'
-    return csv
-
-
-def main(file_name):
-    table_csv = get_table_csv_results(file_name)
-
-    output_file = 'output.csv'
-
-    # replace content
-    with open(output_file, "wt") as fout:
-        fout.write(table_csv)
-    pass
-
+from textract import *
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -113,10 +23,10 @@ def getImage():
         binary_file_data = binary_file.read()
         main(binary_file_data)
 
-    file = open('output.csv', 'rb')
+    final_file = open('output.csv', 'rb')
     os.remove(filename)
     os.remove('output.csv')
-    return send_file(file, download_name="output.csv", as_attachment=True, mimetype="text/csv")
+    return send_file(final_file, download_name="output.csv", as_attachment=True, mimetype="text/csv")
 
 
 app.run()
